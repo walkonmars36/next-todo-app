e81042963c82d85857de0112d74e2c7c4f8420f4
Mark Lawson <mark@walkonmars.co.uk>
2023-05-16 15:32:02 +0100

    added _document.js

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -22,7 +22,15 @@ const ToggleButton = styled.button`
   border-radius: calc(var(--toggle-width) / 2);
   cursor: pointer;
   background: var(--color-bg-toggle);
-  transition: background 0.25s ease-in-out;
+  transition: background 0.25s ease-in-out, box-shadow 0.25s ease-in-out;
+  &:focus {
+    outline-offset: 4px;
+  }
+    &:focus:not(:focus-visible) {
+    outline: none;
+    }
+    &:hover {
+        box-shadow: 0 0 5px 2px var(--color-bg-toggle);
 `;
 
 const ToggleThumb = styled.span`
@@ -45,16 +53,27 @@ const ThemeToggle = () => {
   const inactiveTheme = activeTheme === "light" ? "dark" : "light";
 
   useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme");
+    localTheme && setActiveTheme(localTheme);
+  }, []);
+
+  useEffect(() => {
     document.body.dataset.theme = activeTheme;
+    window.localStorage.setItem("theme", activeTheme);
   }, [activeTheme]);
 
   return (
-    <ToggleButton type="button" onClick={() => setActiveTheme(inactiveTheme)}>
+    <ToggleButton
+      aria-label={`Change to ${inactiveTheme} mode`}
+      title={`Change to ${inactiveTheme} mode`}
+      type="button"
+      onClick={() => setActiveTheme(inactiveTheme)}
+    >
       <ToggleThumb activeTheme={activeTheme} />
-      <span>
+      <span aria-hidden={true}>
         <Image src={IconMoon} alt="Moon icon for dark mode" />
       </span>
-      <span>
+      <span aria-hidden={true}>
         <Image src={IconSun} alt="Sun icon for light mode" />
       </span>
     </ToggleButton>
